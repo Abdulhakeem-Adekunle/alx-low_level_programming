33a269839dffa9173bb1eb1ddb3c5159ae40d82d
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-09 14:10:48 -0700

    C program that prints on the screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * File: 4-put.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
