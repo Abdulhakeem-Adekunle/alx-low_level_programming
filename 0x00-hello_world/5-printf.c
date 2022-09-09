ec4a2afa7d7fcebe95a4e01d759b487c8afd9d0d
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-09 14:30:37 -0700

    c program that prints on screen

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File: 5-printf.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art"
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
