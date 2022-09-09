bf5dd5fbe0041a2f2287c68d8258c144c7effee3
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-09 15:47:04 -0700

    C program that prints the size of various types on the computer is it compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,14 @@
 #include <stdio.h>
 
 /**
- * main - "Prints the size of various types based on the computer it is compiled and run on"
+ * main - "Prints the size of various types based on its computer"
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
+
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
 	printf("size of an int: %zu byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
@@ -20,4 +21,5 @@ int main(void)
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
+
 }
