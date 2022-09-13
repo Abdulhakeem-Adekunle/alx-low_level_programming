b4aab0a67e8f660b6ef051bb96a676306d4ebe0f
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-13 15:16:41 -0700

    program that prints the string "_putchar", followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - "write a program that prints _putchar, followed by a new line"
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
