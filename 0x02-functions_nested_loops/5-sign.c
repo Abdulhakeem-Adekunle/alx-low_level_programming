d76ddd27cdd9d186a37111b98924c99118951a23
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-14 15:04:52 -0700

    function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+/*
+ * File: 5-sign.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - "Function that prints the sign of a number"
+ * @n: function parameter
+ *
+ * Return: Always 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
