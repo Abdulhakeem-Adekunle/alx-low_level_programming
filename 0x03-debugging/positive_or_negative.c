e6a6eed73bd2cc26147dba4d771e5e2373d2afe1
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-15 12:02:50 -0700

    program that checks for positive of negative integer

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,36 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - "program that will assign a random number to the
+ * variable n each time it is executed."
+ * @n: Function formal parameter
+ *
+ * Return: Always (0)
+ *
+ */
+int positive_or_negative(int n)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
+}
