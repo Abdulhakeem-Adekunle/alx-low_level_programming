f52cc43380380dce6b34d4c1b2c51634fe0400a9
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-14 18:25:53 -0700

    functions that adds two numbers and return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+/*
+ * File: 10-add.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include "main.h"
+
+/**
+ * add - "Function that add two numbers"
+ * @a: function formal parameter
+ * @b: function formal parameter
+ *
+ * Return: Always 0
+ *
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
