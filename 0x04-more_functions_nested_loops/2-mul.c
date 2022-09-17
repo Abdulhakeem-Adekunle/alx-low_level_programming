c29d7a602cc1eab84d3edcb358465980c1047621
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-17 15:24:23 -0700

    function that multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-mul.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include "main.h"
+
+/**
+ * mul - "function that multiplies two numbers"
+ * @a: function formal parameter one
+ * @b: function formal parameter two
+ *
+ * Return: Always 0
+ *
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
