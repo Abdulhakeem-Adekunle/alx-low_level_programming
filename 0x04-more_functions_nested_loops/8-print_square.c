a94242857c5d71c7830b4d64b1c33f8a2d4aed02
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-18 07:16:15 -0700

    function that prints square of n size

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,12 +17,19 @@ void print_square(int size)
 {
 	int row, col;
 
-	for (col = 1; col <= size; col++)
+	if (size > 0)
 	{
+		for (col = 1; col <= size; col++)
+		{
 		for (row = 1; row <= size; row++)
 		{
 			_putchar('#');
 		}
+		_putchar('\n');
+		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
