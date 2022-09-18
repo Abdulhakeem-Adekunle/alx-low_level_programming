3442396b970adb770c0be2383a8816e3db9c7be1
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-17 17:07:41 -0700

    function that prints 10 times the numbers, from 0 to 14, followed by a new line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+/*
+ * File: 5-more_numbers.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include "main.h"
+
+/**
+ * more_numbers - "function that prints 10 time the numbers, from 0 - 14
+ * followed by a new line"
+ *
+ * Return: Always 0
+ *
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int count;
+
+	for (count = 0; count <= 10; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
