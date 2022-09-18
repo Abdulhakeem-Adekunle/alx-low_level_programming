d4291acace3e79f347f165e46ea4809b53e3e38f
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-18 07:01:19 -0700

    funtions that prints n square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+/*
+ * File: 8-print_square.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include "main.h"
+
+/**
+ * print_square - "Function that prints a square, followed by a new line"
+ * @size: funtion formal parameter
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_square(int size)
+{
+	int row, col;
+
+	for (col = 1; col <= size; col++)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
