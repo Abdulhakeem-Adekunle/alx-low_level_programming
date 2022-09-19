29902c224cea766c8c96352820a31d1072112e9f
Abdulhakeem Yisa <y_kunle@yahoo.com>
2022-09-19 15:41:40 -0700

    function that takes a pointer to an int parameter and updates its the value it points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Abdulhakeem A Yisa
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as a parameter
+ * and updates the value it points to to 98.
+ * @n: function formal parameter
+ *
+ * Return: nothing
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+This project is about programming with C language as a beginner.
+
+It includes some C programming code for program modularization and more  functionalities:
+Working with "pointers'
+Working with "arrays"
+Working with "strings"
+Working with "functions"
